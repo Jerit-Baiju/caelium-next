fb1f480a986fc94deb46ecd716c370df2f2a7b33
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-10 14:50:54 +0530

    fixed bugs

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className='h-screen -m-20 flex flex-col items-center justify-center'>
-      <h1 className='text-4xl md:text-6xl font-bold mb-4'>Your Family's Digital Hub for Memories and Connections</h1>
+      <h1 className='text-4xl md:text-6xl font-bold mb-4'>Your Family&apos;s Digital Hub for Memories and Connections</h1>
       <p className='text-lg md:text-xl mb-6'>Unite, Share, and Cherish Every Moment Together with Caelium.</p>
     </main>
   );
