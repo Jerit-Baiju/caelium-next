22c5627877293721556fdf1d21fd4db02e1edf3e
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-10 15:01:30 +0530

    fixed centering

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className='h-screen -my-20 flex flex-col items-center justify-center'>
+    <main className='h-screen -my-20 flex flex-col items-center justify-center text-center'>
       <h1 className='text-4xl md:text-6xl font-bold mb-4'>Your Family&apos;s Digital Hub for Memories and Connections</h1>
       <p className='text-lg md:text-xl mb-6'>Unite, Share, and Cherish Every Moment Together with Caelium.</p>
     </main>
