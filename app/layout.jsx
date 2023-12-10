b8fa6cf49a10cb6d2f9df649c6be24917e22b236
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-10 17:22:06 +0530

    sample layout set

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import NavBar from '@/components/NavBar';
+import SideBar from '@/components/SideBar';
 import { Handlee } from 'next/font/google';
 import './globals.css';
 
@@ -13,9 +14,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={handlee.className}>
+        <link href='https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css' rel='stylesheet' />
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'
+        />
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js'></script>
         <main className='min-h-screen'>
           <NavBar />
-          <div className='pt-20'>{children}</div>
+          <SideBar />
+          <div class='p-4 sm:ml-64 pt-20'>
+            <div class='p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700'>{children}</div>
+          </div>
         </main>
       </body>
     </html>
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,48 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <main className='h-screen -my-20 flex flex-col items-center justify-center text-center'>
-      <h1 className='text-4xl md:text-6xl font-bold mb-4'>Your Family&apos;s Digital Hub for Memories and Connections</h1>
-      <p className='text-lg md:text-xl mb-6'>Unite, Share, and Cherish Every Moment Together with Caelium.</p>
-    </main>
+    <div className='container mx-auto p-4'>
+      <div className='flex justify-between items-center mb-8'>
+        <h1 className='text-3xl font-semibold'>Welcome, Jerit!</h1>
+        <Link href='/profile' className='text-blue-500 hover:underline'>
+          Edit Profile
+        </Link>
+      </div>
+
+      {/* Overview Widgets */}
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        {/* Upcoming Events */}
+        <div className='bg-white p-4 rounded shadow'>
+          <h2 className='text-lg font-semibold mb-4'>Upcoming Events</h2>
+          {/* Display upcoming events here */}
+          {/* You can use a calendar library for a more interactive display */}
+        </div>
+
+        {/* Quick Actions */}
+        <div className='bg-white p-4 rounded shadow'>
+          <h2 className='text-lg font-semibold mb-4'>Quick Actions</h2>
+          {/* Add buttons for quick actions */}
+          <button className='btn-primary mb-2'>Add Memory</button>
+          <button className='btn-primary mb-2'>Create Todo</button>
+          <button className='btn-primary'>Start Chat</button>
+        </div>
+
+        {/* Featured Content */}
+        <div className='bg-white p-4 rounded shadow'>
+          <h2 className='text-lg font-semibold mb-4'>Featured Content</h2>
+          {/* Display featured photos, blog entries, or collaborative art */}
+          {/* You can use carousels or other interactive components */}
+        </div>
+      </div>
+
+      {/* Global Unity Message */}
+      <div className='bg-blue-100 p-4 rounded mt-8'>
+        <h2 className='text-lg font-semibold mb-2 text-blue-800'>Global Unity Message</h2>
+        <p className='text-gray-700'>
+          "Let's make the world feel like one big family through Caelium. Share your stories and connect with others globally."
+        </p>
+      </div>
+    </div>
   );
 }
