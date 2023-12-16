3402d8a64822a2912597b7b34a439e1dc9513f42
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-16 16:59:04 +0530

    converted to typescrpt

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'media',
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
@@ -16,7 +15,6 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
 }
+export default config
