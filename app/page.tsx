c22e2c6c292467ab4c6cb7d140c83ff0f097388d
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-16 17:49:10 +0530

    fixed bugs when migrated to typescript

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Wrapper from './Wrapper';
 export default function Home() {
   return (
     <Wrapper>
-        <div className='container mx-auto p-4'>
-          <div className='flex justify-between items-center mb-8'>
-            <h1 className='text-3xl font-semibold dark:text-white'>Welcome, Jerit!</h1>
-          </div>
+      <div className='container mx-auto p-4'>
+        <div className='flex justify-between items-center mb-8'>
+          <h1 className='text-3xl font-semibold dark:text-white'>Welcome, Jerit!</h1>
         </div>
-      </Wrapper>
+      </div>
+    </Wrapper>
   );
 }
