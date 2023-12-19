ea66e4b6220f1e66778b90ffb3335956f1c26784
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-19 19:02:34 +0530

    jwtDecode bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
+import { AuthProvider } from '@/contexts/AuthContext';
 import Wrapper from './Wrapper';
 
 export default function Home() {
   return (
-    <Wrapper>
-      <div className='container mx-auto p-4'>
-        <div className='flex justify-between items-center mb-8'>
-          <h1 className='text-3xl font-semibold dark:text-white'>Welcome, Jerit!</h1>
+    <AuthProvider>
+      <Wrapper>
+        <div className='container mx-auto p-4'>
+          <div className='flex justify-between items-center mb-8'>
+            <h1 className='text-3xl font-semibold dark:text-white'>Welcome, Jerit!</h1>
+          </div>
         </div>
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </AuthProvider>
   );
 }
--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,18 +1,18 @@
 'use client';
+import AuthContext from '@/contexts/AuthContext';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useContext } from 'react';
 
 interface InputProps {
   name: string;
   type: string;
   placeholder: string;
   id?: string;
-  setState?: any;
   autofocus?: boolean;
   required?: boolean;
 }
 
-export const Input: React.FC<InputProps> = ({ name, type, placeholder, id, setState, autofocus=false, required=false }) => {
+export const Input: React.FC<InputProps> = ({ name, type, placeholder, id, autofocus = false, required = false }) => {
   return (
     <div className='max-w-sm mx-auto mb-4'>
       <label htmlFor={id} className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
@@ -25,39 +25,26 @@ export const Input: React.FC<InputProps> = ({ name, type, placeholder, id, setSt
         placeholder={placeholder}
         required={required}
         autoFocus={autofocus}
-        onChange={(e) => setState(e.target.value)}
       />
     </div>
   );
 };
 
 const Auth = (props: { page?: any }) => {
+  let { loginUser } = useContext(AuthContext);
   let { page } = props;
-  let [email, setEmail] = useState();
-  let [password, setPassword] = useState();
-
-  let submit = async (e: any) => {
-    e.preventDefault();
-    let response = await fetch('http://127.0.0.1:8000/api/token/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({'username': email, 'password': password})
-    })
-    console.log(response)
-  };
-
   return (
     <div className='flex flex-col items-center justify-center h-screen dark:text-white'>
       <div className='flex justify-center flex-col items-center border-2 border-gray-400 p-10 rounded-lg'>
         <h1 className='text-4xl font-bold mb-4'>{page === 'login' ? 'Welcome Back to Caelium' : 'Create Your Caelium Account'}</h1>
-        <form onSubmit={submit} className='w-full'>
-          <Input setState={setEmail} name='Email' type='email' id='emailId' placeholder='Enter your email' autofocus />
-          <Input setState={setPassword} name='Password' type='password' id='password' placeholder='Enter your password' autofocus/>
+        <form onSubmit={page === 'login' ? loginUser : null} className='w-full'>
+          <div className='p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400' role='alert'>
+            {/* <span className='font-extrabold'>{error}</span> */}
+          </div>
+          <Input name='Username' type='text' id='usernameID' placeholder='Enter your username' autofocus />
+          <Input name='Password' type='password' id='password' placeholder='Enter your password' />
           <div className='flex justify-center flex-col items-center'>
             <button
-              onClick={(e) => submit(e)}
               className='bg-gray-600 text-white px-6 py-3 mt-4 rounded-3xl font-extrabold w-1/3 hover:bg-gray-300 hover:text-black'
               type='submit'>
               {page === 'login' ? 'Log In' : 'Sign Up'}
