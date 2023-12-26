e17b66afb391f9e9f580e062aaa7682de4d2ed7b
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-26 20:46:25 +0530

    removed old cdns

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { AuthProvider } from '@/contexts/AuthContext';
 import type { Metadata } from 'next';
-import Script from 'next/script';
 import { handleeFont } from './font';
 import './globals.css';
 
@@ -15,7 +14,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta name='mobile-web-app-capable' content='yes' />
         <link href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=optional' rel='stylesheet' />
-        <Script src='https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js'></Script>
       </head>
       <body className={handleeFont.className}>
         <AuthProvider>{children}</AuthProvider>
