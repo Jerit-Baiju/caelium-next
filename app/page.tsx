05d56e044b53f3663990aecf2566997da66f1fa8
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-27 12:45:16 +0530

    sample home element

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,12 @@ export default function Home() {
   return (
     <Wrapper>
       <div className='container mx-auto p-4'>
-        <div className='flex justify-between items-center mb-8'>
-          <h1 className='text-3xl font-semibold dark:text-white'>Welcome, {user.name}</h1>
-        </div>
+          <span className='mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl'>
+            <span className='text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400'>Welcome, {user.name}</span>
+          </span>
+          <p className='text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400'>
+            Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.
+          </p>
       </div>
     </Wrapper>
   );
