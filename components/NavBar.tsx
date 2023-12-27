d399d1fe5c9d34b827cd5c93268af5ec733c2ba0
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-27 11:03:10 +0530

    logout modal added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -82,12 +82,13 @@ const NavBar = () => {
                       </Link>
                     </li>
                     <li>
-                      <Link
-                        href='#'
+                      <a
+                        data-modal-target='logout-modal'
+                        data-modal-toggle='logout-modal'
                         className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white'
-                        role='menuitem'>
+                        type='button'>
                         Sign out
-                      </Link>
+                      </a>
                     </li>
                   </ul>
                 </div>
