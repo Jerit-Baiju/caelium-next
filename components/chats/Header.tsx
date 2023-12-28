025c7a1e9eb5fa8d001980b2602744cd4ce3396e
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-28 18:44:13 +0530

    fix: dynamic dark theme

---
+++ components/chats/Header.tsx
@@ -0,0 +1,10 @@
+import AuthContext from '@/contexts/AuthContext';
+// import Image from 'next/image';
+import { useContext } from 'react';
+
+const Header = () => {
+  // let { user } = useContext(AuthContext);
+  return <div className='dark:text-white dark:bg-gray-900 h-16'></div>;
+};
+
+export default Header;
