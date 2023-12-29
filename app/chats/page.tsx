ab3cc82433da6ee538e47490064dadcdd3d5b975
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-29 22:05:50 +0530

    add: sample chat

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,6 +1,9 @@
 import ChatsPane from '@/components/chats/ChatsPane';
 import SpeedDial from '@/components/chats/elements.tsx/SpeedDial';
+import { Comforter } from 'next/font/google';
 import Wrapper from '../Wrapper';
+
+const comforter = Comforter({ weight: '400', subsets: ['cyrillic'] });
 
 const page = () => {
   return (
@@ -10,7 +13,13 @@ const page = () => {
           <ChatsPane />
         </div>
         <div className='flex max-sm:hidden flex-none flex-grow sm:w-3/4 p-5'>
-          <p className='text-white text-6xl'>Hello world</p>
+          <div className='flex flex-col h-full flex-grow items-center justify-center'>
+            <p
+              className={`py-12 text-center text-9xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 ${comforter.className}`}>
+              Caelium
+              <p className='text-6xl'>Elevating your Chat Experience</p>
+            </p>
+          </div>
         </div>
       </div>
       <SpeedDial />
