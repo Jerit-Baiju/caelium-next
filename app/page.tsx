a8a1d951af5bbc899abe1b3f9337bd2f469794f7
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-30 10:02:00 +0000

    optimize: login, logout

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ export default function Home() {
   return (
     <Wrapper>
       <div className='container mx-auto p-4'>
-          <span className='mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl'>
-            <span className='text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400'>Welcome, {user?.name}</span>
-          </span>
-          <p className='text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400'>
-            Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.
-          </p>
+        <span className='mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl'>
+          <span className='text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400'>Welcome, {user?.name}</span>
+        </span>
+        <p className='text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400'>
+          Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.
+        </p>
       </div>
     </Wrapper>
   );
