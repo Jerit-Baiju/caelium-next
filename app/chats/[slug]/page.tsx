d04d8335e5c499d644abe7c6153923f3e42ffe9c
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-30 23:35:30 +0530

    fix: build issues, pending; chat layout

--- app/chats/[slug]/page.tsx
+++ app/chats/[slug]/page.tsx
@@ -6,14 +6,13 @@ import ChatInput from '@/components/chats/elements.tsx/ChatInput';
 import ChatMain from '@/components/chats/elements.tsx/ChatMain';
 
 const page = ({ params }: { params: { slug: string } }) => {
-  // let { user } = useContext(AuthContext);
   return (
     <Wrapper navSM={false}>
-      <div className='flex relative flex-grow sm:divide-x divide-dashed divide-gray-500'>
+      <div className='flex flex-grow sm:divide-x divide-dashed divide-gray-500 overflow-y-scroll'>
         <div className='max-sm:hidden flex flex-grow flex-none sm:w-1/4'>
           <ChatsPane />
         </div>
-        <div className='flex flex-col relative flex-grow sm:w-3/4'>
+        <div className='flex  flex-col flex-grow h-screen sm:w-3/4'>
           <ChatHeader />
           <ChatMain />
           <ChatInput />
--- components/chats/elements.tsx/ChatMain.tsx
+++ components/chats/elements.tsx/ChatMain.tsx
@@ -9,19 +9,17 @@ const ChatMain = () => {
     chatContainerRef.current?.scrollTo(0, chatContainerRef.current.scrollHeight);
   }, []);
   return (
-    <div ref={chatContainerRef} className='flex flex-col overflow-y-scroll flex-grow-0 h-screen -my-[3.82rem] max-h-min text-white'>
-      <div className='py-16'>
-        <TextLeft />
-        <TextLeft />
-        <TextLeft />
-        <TextLeft />
-        <TextLeft />
-        <TextLeft />
-        <TextLeft />
-        <TextLeft />
-        <TextLeft />
-        <TextLeft />
-      </div>
+    <div ref={chatContainerRef} className='flex flex-col h-full overflow-y-scroll'>
+      <TextLeft />
+      <TextLeft />
+      <TextLeft />
+      <TextLeft />
+      <TextLeft />
+      <TextLeft />
+      <TextLeft />
+      <TextLeft />
+      <TextLeft />
+      <TextLeft />
     </div>
   );
 };
