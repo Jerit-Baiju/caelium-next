4fe75540bb45e6770ff190515826e57c091e1909
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-31 15:57:33 +0530

    feat: new chat popup

--- helpers/api.tsx
+++ helpers/api.tsx
@@ -1,15 +1,8 @@
-import axios from 'axios';
-
-export interface UserData {
-  id: number;
-  username: string;
-  name: string;
-  avatar: string;
+export interface GetUrlProps {
+  url: string;
+  method?: string;
 }
 
-export const fetchUser = async (pk: number): Promise<UserData> => {
-  const options = { method: 'GET', url: `${process.env.NEXT_PUBLIC_API_HOST}/api/auth/accounts/${pk}/` };
-  const { data } = await axios.request(options);
-  console.log(data);
-  return data;
+export const getUrl = ({ url, method = 'GET' }: GetUrlProps) => {
+  return { method: method, url: `${process.env.NEXT_PUBLIC_API_HOST}${url}` };
 };
