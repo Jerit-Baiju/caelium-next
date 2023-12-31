27f5820da4c73f22f605c67a351f62696d657cea
Jerit Baiju <jeritalumkal@gmail.com>
2023-12-31 15:57:33 +0530

    feat: new chat popup

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,12 +1,29 @@
+'use client';
+
 import ChatsPane from '@/components/chats/ChatsPane';
 import SpeedDial from '@/components/chats/elements.tsx/SpeedDial';
+import { UserProps, getUrl } from '@/helpers/api';
+import axios from 'axios';
 import { Comforter } from 'next/font/google';
+import { useEffect, useState } from 'react';
 import Wrapper from '../Wrapper';
 import { handleeFont } from '../font';
 
 const comforter = Comforter({ weight: '400', subsets: ['cyrillic'] });
 
 const page = () => {
+  let [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.request(getUrl({ url: '/api/auth/accounts/' }));
+        setUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <Wrapper>
       <div className='flex flex-grow divide-x divide-dashed divide-gray-500'>
@@ -43,70 +60,37 @@ const page = () => {
                 type='button'
                 className='end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white'
                 data-modal-hide='authentication-modal'>
-                <svg className='w-3 h-3' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 14 14'>
-                  <path stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6' />
-                </svg>
+                <span className='material-symbols-outlined'>close</span>
                 <span className='sr-only'>Close modal</span>
               </button>
             </div>
             <div className='p-4 md:p-5'>
               <form className='space-y-4' action='#'>
                 <div>
-                  <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-                    Your email
-                  </label>
                   <input
                     type='email'
                     name='email'
                     id='email'
                     className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
-                    placeholder='name@company.com'
+                    placeholder='search'
                     required
                   />
                 </div>
-                <div>
-                  <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-                    Your password
-                  </label>
-                  <input
-                    type='password'
-                    name='password'
-                    id='password'
-                    placeholder='••••••••'
-                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
-                    required
-                  />
-                </div>
-                <div className='flex justify-between'>
-                  <div className='flex items-start'>
-                    <div className='flex items-center h-5'>
-                      <input
-                        id='remember'
-                        type='checkbox'
-                        value=''
-                        className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800'
-                        required
-                      />
-                    </div>
-                    <label htmlFor='remember' className='ms-2 text-sm font-medium text-gray-900 dark:text-gray-300'>
-                      Remember me
-                    </label>
-                  </div>
-                  <a href='#' className='text-sm text-blue-700 hover:underline dark:text-blue-500'>
-                    Lost Password?
-                  </a>
-                </div>
-                <button
-                  type='submit'
-                  className='w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
-                  Login to your account
-                </button>
-                <div className='text-sm font-medium text-gray-500 dark:text-gray-300'>
-                  Not registered?{' '}
-                  <a href='#' className='text-blue-700 hover:underline dark:text-blue-500'>
-                    Create account
-                  </a>
-                </div>
+                <ul className='sm:max-h-[calc(100dvh-25rem)] overflow-y-scroll'>
+                  {users.map((user: UserProps) => (
+                    <li className='px-3 py-3 m-1 rounded-md hover:bg-gray-800'>
+                      <div className='flex items-center space-x-3 rtl:space-x-reverse'>
+                        <div className='flex-shrink-0'>
+                          <img className='w-12 h-12 rounded-full' src={user.avatar} alt={user.name} />
+                        </div>
+                        <div className='flex-1 min-w-0'>
+                          <p className='text-sm font-semibold text-gray-900 truncate dark:text-white'>{user.username}</p>
+                          <p className='text-sm text-gray-500 truncate dark:text-gray-400'>{user.name}</p>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
               </form>
             </div>
           </div>
