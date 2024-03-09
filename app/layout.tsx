1176b0fa6f5fe7802b3736bbb2034b354d1c5db2
Jerit Baiju <jeritalumkal@gmail.com>
2024-03-09 20:59:11 +0530

    change: tagline in meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import { AuthProvider } from '@/contexts/AuthContext';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { handleeFont } from './font';
 import './globals.css';
-import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Caelium',
-  description: 'Caelium: Your all-in-one life planner for shared moments and organized events. Join now!',
+  description: 'Caelium: Unveil Your World, Connect Your Dreams – Where Privacy Meets Possibility.',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -15,7 +15,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta name='mobile-web-app-capable' content='yes' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0, interactive-widget=resizes-content'></meta>
-        {/* <link href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&display=optional' rel='stylesheet' /> */}
       </head>
       <body className={`${handleeFont.className} bg-background`}>
         <AuthProvider>{children}</AuthProvider>
