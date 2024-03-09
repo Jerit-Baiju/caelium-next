93f116917c7ebb0d0c95e5199fbd073a67a40162
Jerit Baiju <jeritalumkal@gmail.com>
2024-03-09 20:50:22 +0530

    style: theme changed

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -7,9 +7,9 @@ const Page = () => {
       <h1 className='text-center text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400'>
         Welcome to Caelium
       </h1>
-      <p className='text-xl text-center mb-8'>Your Family&apos;s Digital Hub for Memories and Connections</p>
+      <p className='text-xl text-center mb-8'>Unveil Your World, Connect Your Dreams - Where Privacy Meets Possibility.</p>
       <Link href='/accounts/login' className='bg-white text-gray-800 px-6 py-3 rounded-full font-bold hover:bg-blue-100'>
-        Get Started
+        Get Started <i className='fa-solid fa-arrow-right ps-1'></i>
       </Link>
     </div>
   );
