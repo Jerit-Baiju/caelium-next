1594e3bbe8a6dc88677e19eaafa8290df0e0f0a1
Jojit Thomas <103530443+Jojit-Thomas@users.noreply.github.com>
2024-03-26 23:40:18 +0530

    feat: add calendar route with basic coming soon page
    - Added calendar route to prevent 404 errors, displaying a basic "coming soon" page.

---
+++ app/calendar/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Wrapper from '../Wrapper';
+
+const CalendarPage = () => {
+  return (
+    <Wrapper>
+      <div className='p-4 grid place-items-center w-full'>
+        <div className='w-min'>
+          <h1 className='text-5xl'>Calendar</h1>
+          <p className='text-center'>Coming soon...</p>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default CalendarPage;
