4134a11ffbe5d0399441149e471b95939904d94b
Jerit Baiju <jeritalumkal@gmail.com>
2024-03-26 19:01:28 +0530

    add: truncate and getTime helper functions

--- helpers/support.tsx
+++ helpers/support.tsx
@@ -21,3 +21,13 @@ export const getUrl = ({ url, method = 'GET', data, token }: GetUrlProps) => {
 export const getMedia = (path: string) => {
   return `${process.env.NEXT_PUBLIC_API_HOST + '/' + path}`;
 };
+
+export const truncate = ({ chars, length }: { chars: string | null; length: number }) => {
+  return chars ? `${chars?.substring(0, length)}...` : '';
+};
+
+export const getTime = (timestamp: string | null) => {
+  const date = new Date(timestamp ? timestamp : '');
+  const formatted = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+  return timestamp ? formatted : '';
+};
