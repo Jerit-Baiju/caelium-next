e31e2ac43a6bba7c1e229e96079123f068ed0343
Jerit Baiju <jeritalumkal@gmail.com>
2024-03-27 12:31:01 +0530

    format: using prettier

--- contexts/ChatContext.tsx
+++ contexts/ChatContext.tsx
@@ -1,9 +1,9 @@
-"use client";
-import { User } from "@/helpers/props";
-import { getUrl } from "@/helpers/support";
-import axios, { AxiosError } from "axios";
-import { ReactNode, createContext, useContext, useEffect, useRef, useState } from "react";
-import AuthContext from "./AuthContext";
+'use client';
+import { User } from '@/helpers/props';
+import { getUrl } from '@/helpers/support';
+import axios, { AxiosError } from 'axios';
+import { ReactNode, createContext, useContext, useEffect, useRef, useState } from 'react';
+import AuthContext from './AuthContext';
 
 interface childrenProps {
   chatId: Number;
@@ -20,7 +20,7 @@ interface ChatContextProps {
 
 const ChatContext = createContext<ChatContextProps>({
   handleSubmit: async () => {},
-  textInput: "",
+  textInput: '',
   setTextInput: async () => {},
   messages: [],
 });
@@ -28,12 +28,12 @@ export default ChatContext;
 
 type ErrorType = {
   text: string;
-  code: "CHAT_NOT_FOUND" | "FETCH_MESSAGES_FAILED";
+  code: 'CHAT_NOT_FOUND' | 'FETCH_MESSAGES_FAILED';
 };
 
 export const ChatProvider = ({ chatId, children }: childrenProps) => {
   const { authTokens } = useContext(AuthContext);
-  const [textInput, setTextInput] = useState("");
+  const [textInput, setTextInput] = useState('');
   const [messages, setMessages] = useState([]);
   const [recipient, setRecipient] = useState<User>();
   const [error, setError] = useState<ErrorType | null>(null);
@@ -47,19 +47,19 @@ export const ChatProvider = ({ chatId, children }: childrenProps) => {
     // Define event handlers for the WebSocket
     socket.current.onmessage = function (e) {
       let data = JSON.parse(e.data);
-      if (data["type"] === "message") {
-        console.log(data["message"]);
+      if (data['type'] === 'message') {
+        console.log(data['message']);
       }
     };
     socket.current.onopen = function () {
-      console.log("socket opened");
+      console.log('socket opened');
     };
 
     // Cleanup function to close the WebSocket connection when the component unmounts
     return () => {
       if (socket.current) {
         socket.current.close();
-        console.log("socket closed");
+        console.log('socket closed');
       }
     };
   }, [chatId]);
@@ -69,22 +69,20 @@ export const ChatProvider = ({ chatId, children }: childrenProps) => {
     socket.current?.send(
       JSON.stringify({
         content: textInput,
-      })
+      }),
     );
-    setTextInput("");
+    setTextInput('');
   };
 
   useEffect(() => {
     const fetchMessages = async () => {
       try {
-        const response = await axios.request(
-          getUrl({ url: `/api/chats/messages/${chatId}/`, token: authTokens?.access })
-        );
+        const response = await axios.request(getUrl({ url: `/api/chats/messages/${chatId}/`, token: authTokens?.access }));
         setMessages(response.data);
       } catch (error) {
-        if (error instanceof AxiosError && error.code === "ERR_BAD_REQUEST")
-          setError({ text: "Failed to fetch messages", code: "FETCH_MESSAGES_FAILED" });
-        console.error("Error fetching data:", error);
+        if (error instanceof AxiosError && error.code === 'ERR_BAD_REQUEST')
+          setError({ text: 'Failed to fetch messages', code: 'FETCH_MESSAGES_FAILED' });
+        console.error('Error fetching data:', error);
       }
     };
     fetchMessages();
@@ -93,14 +91,12 @@ export const ChatProvider = ({ chatId, children }: childrenProps) => {
   useEffect(() => {
     const fetchMessages = async () => {
       try {
-        const response = await axios.request(
-          getUrl({ url: `/api/chats/${chatId}/`, token: authTokens?.access })
-        );
-        setRecipient(response.data["other_participant"]);
+        const response = await axios.request(getUrl({ url: `/api/chats/${chatId}/`, token: authTokens?.access }));
+        setRecipient(response.data['other_participant']);
       } catch (error) {
-        if (error instanceof AxiosError && error.code === "ERR_BAD_REQUEST")
-          setError({ text: "Chat not found", code: "CHAT_NOT_FOUND" });
-        console.error("Error fetching data:", error);
+        if (error instanceof AxiosError && error.code === 'ERR_BAD_REQUEST')
+          setError({ text: 'Chat not found', code: 'CHAT_NOT_FOUND' });
+        console.error('Error fetching data:', error);
       }
     };
     fetchMessages();
@@ -116,9 +112,9 @@ export const ChatProvider = ({ chatId, children }: childrenProps) => {
 
   if (error) {
     return (
-      <div className="flex max-sm:max-h-[calc(100dvh-5rem] flex-col flex-grow h-screen sm:w-3/4">
-        <div className="flex flex-col items-center justify-center flex-grow">
-          <h1 className="text-2xl font-bold">{error.text}</h1>
+      <div className='flex max-sm:max-h-[calc(100dvh-5rem] flex-col flex-grow h-screen sm:w-3/4'>
+        <div className='flex flex-col items-center justify-center flex-grow'>
+          <h1 className='text-2xl font-bold'>{error.text}</h1>
           {/* <p className="text-gray-500">{error.code}</p> */}
         </div>
       </div>
