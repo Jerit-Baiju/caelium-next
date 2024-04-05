35116d1baaa10c7e46884b9b34a7e14945ba2954
Jerit Baiju <jeritalumkal@gmail.com>
2024-04-05 15:58:43 +0530

    update: NavBar component with background color and border for MD view on both light and dark themes

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -29,7 +29,7 @@ const NavBar = () => {
           </button>
         </div>
         <div className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1' id='navbar-sticky'>
-          <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700'>
+          <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-neutral-800 md:dark:bg-neutral-900 dark:border-neutral-700'>
             <li>
               <a
                 href='#'
