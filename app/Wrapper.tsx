008518ad29dd7fbd462eec706b620160e83a971a
Jerit Baiju <jeritalumkal@gmail.com>
2024-04-06 11:08:27 +0530

    add: NavContext to NavBar, refactored CustomSelect option in Home

--- app/Wrapper.tsx
+++ app/Wrapper.tsx
@@ -4,6 +4,8 @@ import NavBar from '@/components/NavBar';
 import SideBar from '@/components/SideBar';
 import { initFlowbite } from 'flowbite';
 import { ReactNode, useEffect } from 'react';
+import { NavProvider } from '@/contexts/NavContext';
+import NavContext from '@/contexts/NavContext';
 
 interface WrapperProps {
   children: ReactNode;
@@ -18,7 +20,9 @@ const Wrapper = ({ children, navSM = true }: WrapperProps) => {
     <main>
       <SideBar />
       <div className={navSM ? '' : 'max-sm:hidden'}>
-        <NavBar />
+        <NavProvider>
+          <NavBar />
+        </NavProvider>
       </div>
       <div className={`md:ml-64 flex flex-col flex-grow ${navSM ? 'mt-20' : 'sm:mt-20 max-sm:h-screen'}`}>{children}</div>
       <div className={navSM ? 'max-sm:mt-16' : 'max-sm:hidden'}>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,10 @@
+import NavContext from '@/contexts/NavContext';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useContext } from 'react';
 
 const NavBar = () => {
+  let { ctaButton, buttons } = useContext(NavContext);
   return (
     <nav className='bg-white dark:bg-neutral-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600'>
       <div className='max-w-screen-xl flex flex-wrap items-center h-20 justify-between mx-auto p-1'>
@@ -9,12 +12,16 @@ const NavBar = () => {
           <Image className='p-0 m-0 pointer-events-none' src={'/logos/written.png'} alt='caelium' width={150} height={0} />
         </Link>
         <div className='flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse'>
-          <button
-            type='button'
-            className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-          >
-            Get started
-          </button>
+          {ctaButton && (
+            <Link
+              href={ctaButton?.url}
+              type='button'
+              className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+            >
+              {ctaButton.name}
+            </Link>
+          )}
+
           <button
             data-collapse-toggle='navbar-sticky'
             type='button'
@@ -30,39 +37,17 @@ const NavBar = () => {
         </div>
         <div className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1' id='navbar-sticky'>
           <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-neutral-800 md:dark:bg-neutral-900 dark:border-neutral-700'>
-            <li>
-              <a
-                href='#'
-                className='block py-2 px-3 text-white rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
-                aria-current='page'
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-              >
-                Contact
-              </a>
-            </li>
+            {buttons?.map((button, index) => (
+              <li key={index}>
+                <Link
+                  href={button.url}
+                  className='block py-2 px-3 text-white rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
+                  aria-current='page'
+                >
+                  {button.name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
