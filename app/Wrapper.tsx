70ab76697e3d72f88acf41d6b7e56798608aed2e
Jerit Baiju <jeritalumkal@gmail.com>
2024-04-06 22:18:13 +0530

    fix: navbar context

--- app/Wrapper.tsx
+++ app/Wrapper.tsx
@@ -2,10 +2,9 @@
 import BottomNav from '@/components/BottomNav';
 import NavBar from '@/components/NavBar';
 import SideBar from '@/components/SideBar';
+import { NavProvider } from '@/contexts/NavContext';
 import { initFlowbite } from 'flowbite';
 import { ReactNode, useEffect } from 'react';
-import { NavProvider } from '@/contexts/NavContext';
-import NavContext from '@/contexts/NavContext';
 
 interface WrapperProps {
   children: ReactNode;
@@ -19,12 +18,12 @@ const Wrapper = ({ children, navSM = true }: WrapperProps) => {
   return (
     <main>
       <SideBar />
-      <div className={navSM ? '' : 'max-sm:hidden'}>
-        <NavProvider>
+      <NavProvider>
+        <div className={navSM ? '' : 'max-sm:hidden'}>
           <NavBar />
-        </NavProvider>
-      </div>
-      <div className={`md:ml-64 flex flex-col flex-grow ${navSM ? 'mt-20' : 'sm:mt-20 max-sm:h-screen'}`}>{children}</div>
+        </div>
+        <div className={`md:ml-64 flex flex-col flex-grow ${navSM ? 'mt-20' : 'sm:mt-20 max-sm:h-screen'}`}>{children}</div>
+      </NavProvider>
       <div className={navSM ? 'max-sm:mt-16' : 'max-sm:hidden'}>
         <BottomNav />
       </div>
