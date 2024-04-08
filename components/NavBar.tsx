11a47b502b30b5179e8cad4d59a44732ebb82f4a
Jerit Baiju <jeritalumkal@gmail.com>
2024-04-08 23:09:43 +0530

    update: NavBar Link component to use flex layout

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -21,7 +21,7 @@ const NavBar = () => {
             <Link
               href={ctaButton.url}
               type='button'
-              className=' bg-blue-500 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+              className='flex items-center justify-center bg-blue-500 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
             >
               {ctaButton.name}
             </Link>
