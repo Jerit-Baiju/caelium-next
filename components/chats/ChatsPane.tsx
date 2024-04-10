0606a19c17eb3a52dda4bc2bf8c8fce8945daefc
Jerit Baiju <jeritalumkal@gmail.com>
2024-04-10 15:43:16 +0530

    remove: User component and related state, add search functionality for chats

--- components/chats/ChatsPane.tsx
+++ components/chats/ChatsPane.tsx
@@ -1,64 +1,48 @@
 'use client';
 import AuthContext from '@/contexts/AuthContext';
-import { Chat, User } from '@/helpers/props';
+import { Chat } from '@/helpers/props';
 import { getMedia, getTime, getUrl, truncate } from '@/helpers/support';
 import axios from 'axios';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import { useContext, useEffect, useState } from 'react';
 
 const ChatsPane = () => {
-  const router = useRouter();
   let { authTokens } = useContext(AuthContext);
   let [chats, setChats] = useState([]);
-  let [users, setUsers] = useState([]);
-  let [showChats, setShowChats] = useState(true);
+  let [search_query, setSearch_query] = useState('');
 
-  const fetchUsers = async (e: any) => {
+  const searchChats = async (e: any) => {
     e.preventDefault();
     try {
-      const response = await axios.request(getUrl({ url: '/api/auth/accounts/', token: authTokens?.access }));
-      setUsers(response.data);
-      console.log(response.data);
-      setShowChats(false);
+      const response = await axios.request(getUrl({ url: `/api/chats/?search=${search_query}`, token: authTokens?.access }));
+      setChats(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
 
-  const createChat = async (recipient_id: number) => {
+  const fetchChats = async () => {
     try {
-      const response = await axios.request(
-        getUrl({
-          url: '/api/chats/',
-          data: { participant: recipient_id },
-          token: authTokens.access,
-          method: 'POST',
-        }),
-      );
-      console.log(response.data);
-      router.push(`/chats/${response.data.id}`);
+      const response = await axios.request(getUrl({ url: '/api/chats/', token: authTokens?.access }));
+      setChats(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
-
   useEffect(() => {
-    const fetchChats = async () => {
-      try {
-        const response = await axios.request(getUrl({ url: '/api/chats/', token: authTokens?.access }));
-        setChats(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
     fetchChats();
   }, [authTokens?.access]);
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === 'Enter' && e.target.value != '') {
+      searchChats(e);
+    }
+  };
 
   return (
     <div className='flex flex-col h-[calc(100dvh-5rem)] w-full flex-grow max-sm:h-min overflow-x-hidden overflow-y-auto'>
       <div className='flex w-full sticky top-0 z-10 flex-col'>
-        <form onSubmit={(e) => fetchUsers(e)} className='m-3'>
+        <form onSubmit={(e) => searchChats(e)} className='m-3'>
           <label htmlFor='default-search' className='mb-2 text-sm font-medium text-neutral-900 sr-only dark:text-white'>
             Search
           </label>
@@ -68,15 +52,33 @@ const ChatsPane = () => {
             </div>
             <input
               type='text'
-              id='default-search'
-              className='block w-full p-2 ps-10 text-sm text-neutral-900 border border-neutral-300 rounded-lg bg-neutral-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-neutral-900 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              id='chat-search'
+              className='block w-full p-2 ps-10 pe-10 text-sm text-neutral-900 border border-neutral-300 rounded-lg bg-neutral-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-neutral-900 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
               placeholder='Search'
+              value={search_query}
+              onChange={(e) => {
+                e.target.value == '' ? fetchChats() : null;
+                setSearch_query(e.target.value);
+              }}
+              onKeyDown={(e) => handleKeyDown(e)}
+              autoComplete='off'
               required
             />
+            {search_query && (
+              <button
+                onClick={() => {
+                  setSearch_query('');
+                  fetchChats();
+                }}
+                className='absolute inset-y-0 end-0 flex items-center w-fit pe-3'
+              >
+                <i className='fa-solid fa-xmark'></i>
+              </button>
+            )}
           </div>
         </form>
       </div>
-      <ul className={`${!showChats && 'max-sm:hidden'}`} role='list'>
+      <ul role='list'>
         {chats.map((chat: Chat) => (
           <Link key={chat.id} href={`/chats/${chat.id}`}>
             <li className='px-3 py-2 m-1 rounded-md hover:bg-neutral-200 dark:hover:bg-neutral-900'>
@@ -102,24 +104,6 @@ const ChatsPane = () => {
               </div>
             </li>
           </Link>
-        ))}
-      </ul>
-      <ul className={`${showChats && 'max-sm:hidden'}`} role='list'>
-        {users.map((user: User) => (
-          <li onClick={() => createChat(user.id)} key={user.id} className='px-3 py-3 m-1 rounded-md hover:bg-neutral-700'>
-            <div className='flex items-center space-x-3 rtl:space-x-reverse'>
-              <div className='flex-shrink-0'>
-                <img className='w-12 h-12 rounded-full dark:bg-white' src={user.avatar} alt={user.name} />
-              </div>
-              <div className='flex-1 min-w-0'>
-                <p className='text-sm font-semibold text-neutral-900 truncate dark:text-white'>{user.name}</p>
-                <p className='text-sm text-neutral-500 truncate dark:text-neutral-400'>{user.username}</p>
-              </div>
-              {/* <span className='inline-flex items-center bg-blue-100 text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-neutral-900 dark:text-neutral-300'>
-                  1
-                </span> */}
-            </div>
-          </li>
         ))}
       </ul>
     </div>
