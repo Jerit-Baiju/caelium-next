b58637702feb8792fe9c73a4f54bd541bcfe156d
Jerit Baiju <jeritalumkal@gmail.com>
2024-04-18 16:40:33 +0530

    remove: unwanted margins

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -18,7 +18,7 @@ interface InputProps {
 export const Input: React.FC<InputProps> = ({ name, label, type, placeholder, id, error, autofocus = false, required = false }) => {
   return (
     <div className='max-w-sm mx-auto mb-4'>
-      <label htmlFor={id} className='input input-bordered flex items-center gap-2 m-4'>
+      <label htmlFor={id} className='input input-bordered flex items-center gap-2'>
         {label}
         <input id={id} name={name} required={required} autoFocus={autofocus} type={type} className='grow' placeholder={placeholder} />
       </label>
