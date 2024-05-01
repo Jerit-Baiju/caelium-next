a9c7063e0523c2aee27b13e255835348a5666726
Jerit Baiju <jeritalumkal@gmail.com>
2024-05-01 21:45:40 +0530

    add: Separator component to distinguish messages from different days in ChatMain

---
+++ components/chats/elements.tsx/ChatBubbles/Separator.tsx
@@ -0,0 +1,31 @@
+const formatTimestamp = (timestamp: Date) => {
+  const now = new Date();
+  const date = new Date(timestamp);
+  const yesterday = new Date(now);
+  yesterday.setDate(yesterday.getDate() - 1);
+  if (date.toDateString() === yesterday.toDateString()) {
+    return 'Yesterday';
+  }
+  const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  if (date.toDateString() === now.toDateString()) {
+    return 'Today';
+  } else if (date.toDateString() === new Date(now.setDate(now.getDate() - 1)).toDateString()) {
+    return 'Yesterday';
+  } else if (date >= new Date(now.setDate(now.getDate() - 6))) {
+    return weekdays[date.getDay()];
+  }
+  const dd = String(date.getDate()).padStart(2, '0');
+  const mm = String(date.getMonth() + 1).padStart(2, '0');
+  const yyyy = date.getFullYear();
+  return `${dd}/${mm}/${yyyy}`;
+};
+
+export const Separator = ({ timestamp }: { timestamp: Date }) => {
+  return (
+    <div className='flex flex-grow items-center justify-center my-2'>
+      <p className='dark:bg-neutral-800 dark:text-neutral-400 bg-neutral-200 text-neutral-800 py-0.5 px-4 rounded-lg'>
+      {formatTimestamp(timestamp)}
+      </p>
+    </div>
+  );
+};
--- components/chats/elements.tsx/ChatMain.tsx
+++ components/chats/elements.tsx/ChatMain.tsx
@@ -1,12 +1,13 @@
 'use client';
 import ChatContext from '@/contexts/ChatContext';
-import { Message } from '@/helpers/props';
-import { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import DocMessage from './ChatBubbles/DocMessage';
 import ImageMessage from './ChatBubbles/ImageMessage';
 import TextMessage from './ChatBubbles/TextMessage';
 import VideoMessage from './ChatBubbles/VideoMessage';
 import VoiceMessage from './ChatBubbles/VoiceMessage';
+import { Message } from '@/helpers/props';
+import { Separator } from './ChatBubbles/Separator';
 
 const ChatMain = () => {
   let { messages } = useContext(ChatContext);
@@ -18,25 +19,54 @@ const ChatMain = () => {
     }
   }, [messages]);
 
+  const isSameDay = (date1:Date, date2:Date) => {
+    return date1.getFullYear() === date2.getFullYear() && date1.getMonth() === date2.getMonth() && date1.getDate() === date2.getDate();
+  };
+
+  let prevDate: Date | null = null;
+
+  const renderMessage = (message:Message) => {
+    if (message.type === 'txt') {
+      return <TextMessage key={message.id} message={message} />;
+    } else if (message.type === 'img') {
+      return <ImageMessage key={message.id} message={message} />;
+    } else if (message.type === 'vid') {
+      return <VideoMessage key={message.id} message={message} />;
+    } else if (message.type === 'aud') {
+      return <VoiceMessage key={message.id} message={message} />;
+    } else if (message.type === 'doc') {
+      return <DocMessage key={message.id} message={message} />;
+    } else {
+      return null;
+    }
+  };
+
   return (
     <div ref={containerRef} className='flex flex-col overflow-auto h-full p-2'>
       <div className='flex-grow' />
       <div className='flex flex-col justify-end'>
         {messages.length === 0 && <div className='text-center text-neutral-400'>No messages yet</div>}
-        {messages.map((message: Message) => {
-          if (message.type === 'txt') {
-            return <TextMessage key={message.id} message={message} />;
-          } else if (message.type === 'img') {
-            return <ImageMessage key={message.id} message={message} />;
-          } else if (message.type === 'vid') {
-            return <VideoMessage key={message.id} message={message} />;
-          } else if (message.type === 'aud') {
-            return <VoiceMessage key={message.id} message={message} />;
-          } else if (message.type === 'doc') {
-            return <DocMessage key={message.id} message={message} />;
-          } else {
-            return null; 
+        {messages.map((message, index) => {
+          // Convert the message date string to a Date object
+          const currentDate = new Date(message.timestamp);
+          // Check if it's a new day compared to the previous message
+          const isNewDay = !prevDate || !isSameDay(prevDate, currentDate);
+
+          // Update the previous date for the next iteration
+          prevDate = currentDate;
+
+          // Render the separator if it's a new day
+          if (isNewDay) {
+            return (
+              <React.Fragment key={`separator-${index}`}>
+                <Separator timestamp={currentDate}/>
+                {renderMessage(message)}
+              </React.Fragment>
+            );
           }
+
+          // Render the message normally if it's not a new day
+          return renderMessage(message);
         })}
       </div>
     </div>
