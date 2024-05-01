4f75db42b9bde10a1852462a552fa248e05de6b4
Jerit Baiju <jeritalumkal@gmail.com>
2024-05-01 20:24:06 +0530

    formatted: remove spaces

--- helpers/support.tsx
+++ helpers/support.tsx
@@ -4,7 +4,6 @@ export const truncate = ({ chars, length }: { chars: string | null; length: numb
 
 export const getTime = (timestamp: string | null | undefined | Date): string => {
   if (!timestamp) return '';
-
   const lastChatDate = new Date(timestamp);
 
   const formatTime = (date: Date): string => {
@@ -14,20 +13,19 @@ export const getTime = (timestamp: string | null | undefined | Date): string => 
     const formattedHours = hours % 12 || 12;
     const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
     return `${formattedHours}:${formattedMinutes} ${amPM}`;
-  }
+  };
 
   const formatDate = (date: Date): string => {
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear().toString().slice(-2);
     return `${day}/${month}/${year}`;
-  }
+  };
 
   const getLastChatTime = (lastChatDate: Date): string => {
     const currentDate = new Date();
     const lastChatDay = lastChatDate.getDate();
     const currentDay = currentDate.getDate();
-  
     if (currentDay === lastChatDay) {
       return formatTime(lastChatDate);
     } else if (currentDay - lastChatDay === 1) {
@@ -35,7 +33,7 @@ export const getTime = (timestamp: string | null | undefined | Date): string => 
     } else {
       return formatDate(lastChatDate);
     }
-  }
+  };
 
   return getLastChatTime(lastChatDate);
-}
+};
