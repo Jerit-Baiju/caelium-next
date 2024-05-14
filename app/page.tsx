732a419ab9bc2fec10e60be22dab6886002ce7b5
Jerit Baiju <jeritalumkal@gmail.com>
2024-05-14 16:46:25 +0530

    add: EventWidget and TaskWidget; remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 'use client';
 import CustomSelect from '@/components/home/CustomSelect';
 import SpeedDial from '@/components/home/SpeedDial';
+import EventWidget from '@/components/home/widgets/EventWidget';
+import TaskWidget from '@/components/home/widgets/TaskWidget';
 import AuthContext from '@/contexts/AuthContext';
-import { Task } from '@/helpers/props';
-import useAxios from '@/helpers/useAxios';
-import { useRouter } from 'next/navigation';
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import Wrapper from './Wrapper';
 
 const spaceOptions = [
@@ -16,34 +15,10 @@ const spaceOptions = [
 ];
 
 export default function Home() {
-  let api = useAxios();
-  const router = useRouter();
   let { user } = useContext(AuthContext);
-  let [tasks, setTasks] = useState<Task[]>([]);
-
-  useEffect(() => {
-    const fetchTasks = async () => {
-      try {
-        const response = await api.get('/api/tasks');
-        setTasks(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchTasks();
-  }, []);
 
   const handleSelect = (value: string) => {
     console.log('Selected Version:', value);
-  };
-
-  const completeTask = async (id: number) => {
-    try {
-      api.patch(`/api/tasks/${id}/`, { completed: true });
-      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
-    } catch (error) {
-      console.log(error);
-    }
   };
 
   return (
@@ -66,37 +41,8 @@ export default function Home() {
               <div className='bg-neutral-800 rounded-lg' />
             </div>
             <div className='grid grid-cols-1 md:grid-cols-4 gap-8 md:pt-8'>
-              <div
-                className='dark:bg-neutral-800 h-72 rounded-lg cursor-pointer'
-                onClick={() => {
-                  router.push('/tasks');
-                }}
-              >
-                <p className='text-3xl text-center border-b my-4 mx-4'>Tasks</p>
-                <div className='overflow-auto h-56'>
-                  {tasks.length != 0 ? (
-                    tasks.map((task: Task, i) => (
-                      <div
-                        key={i}
-                        className='flex flex-grow justify-between items-center dark:bg-neutral-900 mb-4 mx-4 px-4 py-2 rounded-lg'
-                      >
-                        {task.name}
-                        <div className='flex gap-4'>
-                          <i
-                            onClick={() => completeTask(task.id)}
-                            className='fa-solid fa-check text-green-500 text-lg bg-neutral-800 p-1 my-0.5 rounded-sm cursor-pointer'
-                          ></i>
-                        </div>
-                      </div>
-                    ))
-                  ) : (
-                    <div className='flex flex-col items-center justify-center h-full'>
-                      <i className='fa-solid fa-circle-plus text-7xl text-center text-neutral-500' />
-                      <p className='p-2 text-neutral-400 text-xl'>Add Task</p>
-                    </div>
-                  )}
-                </div>
-              </div>
+              <TaskWidget />
+              <EventWidget />
             </div>
           </div>
         </div>
