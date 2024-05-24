d773ed3ff9738be1b3ac2831178248612e898b53
Jerit Baiju <jeritalumkal@gmail.com>
2024-05-24 22:40:19 +0530

    format: removed comments, whitespace

--- components/chats/elements/ChatMain.tsx
+++ components/chats/elements/ChatMain.tsx
@@ -19,13 +19,13 @@ const ChatMain = () => {
     }
   }, [messages]);
 
-  const isSameDay = (date1:Date, date2:Date) => {
+  const isSameDay = (date1: Date, date2: Date) => {
     return date1.getFullYear() === date2.getFullYear() && date1.getMonth() === date2.getMonth() && date1.getDate() === date2.getDate();
   };
 
   let prevDate: Date | null = null;
 
-  const renderMessage = (message:Message) => {
+  const renderMessage = (message: Message) => {
     if (message.type === 'txt') {
       return <TextMessage key={message.id} message={message} />;
     } else if (message.type === 'img') {
@@ -47,25 +47,17 @@ const ChatMain = () => {
       <div className='flex flex-col justify-end'>
         {messages.length === 0 && <div className='text-center text-neutral-400'>No messages yet</div>}
         {messages.map((message, index) => {
-          // Convert the message date string to a Date object
           const currentDate = new Date(message.timestamp);
-          // Check if it's a new day compared to the previous message
           const isNewDay = !prevDate || !isSameDay(prevDate, currentDate);
-
-          // Update the previous date for the next iteration
           prevDate = currentDate;
-
-          // Render the separator if it's a new day
           if (isNewDay) {
             return (
               <React.Fragment key={`separator-${index}`}>
-                <Separator timestamp={currentDate}/>
+                <Separator timestamp={currentDate} />
                 {renderMessage(message)}
               </React.Fragment>
             );
           }
-
-          // Render the message normally if it's not a new day
           return renderMessage(message);
         })}
       </div>
