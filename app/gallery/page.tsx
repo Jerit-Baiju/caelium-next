a08866fff5543520395edd5bbe42ed033ac48a21
Jerit Baiju <jeritalumkal@gmail.com>
2024-06-07 10:13:43 +0530

    add: Loader component, updated image interface, and refactored gallery page to handle loading state and image rendering

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,17 +1,27 @@
 'use client';
+import Loader from '@/components/Loader';
 import { useNavbar } from '@/contexts/NavContext';
-import { Image, NavLink } from '@/helpers/props';
+import { NavLink } from '@/helpers/props';
 import useAxios from '@/helpers/useAxios';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import Wrapper from '../Wrapper';
 
 const Page = () => {
+  interface Image {
+    id: string;
+    url: string;
+    filename: string;
+    timestamp: Date;
+  }
+
   const api = useAxios();
+  const [loading, setLoading] = useState<boolean>(true);
   const { setCtaButton, defaultCtaButton, navLinks, setNavLinks } = useNavbar();
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState<Image[] | null>(null);
   const [havePermission, setHavePermission] = useState<boolean>(false);
   const [accessURL, setAccessURL] = useState<string>('');
+
   useEffect(() => {
     setCtaButton({ name: 'Upload', url: 'gallery/upload' });
     const newLinks: NavLink[] = navLinks ? [...navLinks] : [];
@@ -19,14 +29,17 @@ const Page = () => {
     setNavLinks(newLinks);
 
     const fetchImages = async () => {
-      const response = await api.get('/api/gallery/photos/');
+      const response = await api.get('/api/gallery/');
       if (response.status == 200) {
         setImages(response.data);
+        setLoading(false);
         setHavePermission(true);
+        console.log(response.data);
       } else {
         setAccessURL(response.data.url);
       }
     };
+
     fetchImages();
 
     return () => {
@@ -34,23 +47,29 @@ const Page = () => {
     };
   }, []);
 
-  return (
+  return loading ? (
+    <Wrapper>
+      <div className='flex items-center justify-center h-[calc(100dvh-9rem)]'>
+        <Loader />
+      </div>
+    </Wrapper>
+  ) : (
     <Wrapper>
       <div className='p-4'>
         {havePermission ? (
           <>
             <p className='text-3xl py-2'>Jan 8, 2024</p>
             <div className='grid grid-cols-1 max-sm:grid-cols-2 md:grid-cols-7 gap-4'>
-              {images.map((image: Image, i) => (
+              {images?.map((image: Image, i) => (
                 <Link key={i} href={`/gallery/image/${image.id}`}>
-                  <img className='aspect-square rounded-lg object-cover' key={i} src={image.file} />
+                  <img className='aspect-square rounded-lg object-cover' key={i} src={image.url} />
                 </Link>
               ))}
             </div>
           </>
         ) : (
           <div className='flex flex-col items-center'>
-            <Link href={'http://127.0.0.1:8000/api/gallery/auth/'} className='bg-neutral-700 border-neutral-600 p-2 border rounded-lg'>
+            <Link href={accessURL} className='bg-neutral-700 border-neutral-600 p-2 border rounded-lg'>
               sync google photos
             </Link>
           </div>
