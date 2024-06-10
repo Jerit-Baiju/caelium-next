0cfe7de29765d880d721c530deb19973225ce423
Jerit Baiju <jeritalumkal@gmail.com>
2024-06-10 11:10:41 +0530

    add: keys to mapped elements in Albums component and modified DetailedImage component to use useRouter and replace Link with button for back navigation

--- app/gallery/image/[slug]/page.tsx
+++ app/gallery/image/[slug]/page.tsx
@@ -2,10 +2,12 @@
 import { Image } from '@/helpers/props';
 import useAxios from '@/helpers/useAxios';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 const DetailedImage = ({ params }: { params: { slug: string } }) => {
   const api = useAxios();
+  const router  = useRouter()
   const [image, setImage] = useState<Image | null>();
   useEffect(() => {
     const fetchImage = async () => {
@@ -19,9 +21,9 @@ const DetailedImage = ({ params }: { params: { slug: string } }) => {
       <div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]' />
       <nav className='flex justify-between h-16 fixed w-full z-40 top-0 start-0 p-6'>
         <div className='flex justify-start items-center text-xl'>
-          <Link href={'/gallery'}>
+          <button onClick={()=>{router.back()}}>
             <span className='material-symbols-outlined'>arrow_back</span>
-          </Link>
+          </button>
         </div>
         <div className='flex justify-end items-center text-xl gap-6'>
           <span className='material-symbols-outlined'>share</span>
