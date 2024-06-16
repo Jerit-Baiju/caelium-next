cca0a5bfb4a9f0d73fd9f5f0ae191bc722a45a8a
Jerit Baiju <jeritalumkal@gmail.com>
2024-06-16 14:29:34 +0530

    update: Home page to use state for selected space to conditionally render space component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,56 @@
 'use client';
 import CustomSelect from '@/components/home/CustomSelect';
 import SpeedDial from '@/components/home/SpeedDial';
-import EventWidget from '@/components/home/widgets/EventWidget';
-import TaskWidget from '@/components/home/widgets/TaskWidget';
-import AuthContext from '@/contexts/AuthContext';
-import { useContext } from 'react';
+import { useEffect, useState } from 'react';
 import Wrapper from './Wrapper';
+import Personal from '@/components/home/spaces/personal';
 
 const spaceOptions = [
-  { value: 'Personal', label: 'Personal', icon: 'user' },
-  { value: 'Partner', label: 'Partner', icon: 'heart' },
-  { value: 'Family', label: 'Family', icon: 'people-group' },
-  { value: 'Work', label: 'Work', icon: 'building' },
+  { value: 'personal', label: 'Personal', icon: 'user' },
+  { value: 'partner', label: 'Partner', icon: 'heart' },
+  { value: 'family', label: 'Family', icon: 'people-group' },
+  { value: 'work', label: 'Work', icon: 'building' },
 ];
 
+type SpaceType = 'personal' | 'partner' | 'family' | 'work';
+
 export default function Home() {
-  let { user } = useContext(AuthContext);
-  const handleSelect = (value: string) => {
+   const [selectedSpace, setSelectedSpace] = useState<SpaceType>(() => {
+     const savedSpace = localStorage.getItem('selectedSpace');
+     return (savedSpace as SpaceType) || 'personal';
+   });
+
+  const handleSelect = (value: SpaceType) => {
     console.log('Selected Version:', value);
+    setSelectedSpace(value);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('selectedSpace', selectedSpace);
+  }, [selectedSpace]);
+
+  const renderContent = () => {
+    switch (selectedSpace) {
+      case 'personal':
+        return <Personal/>
+      case 'partner':
+        return <div>Partner Content</div>;
+      case 'family':
+        return <div>Family Content</div>;
+      case 'work':
+        return <div>Work Content</div>;
+      default:
+        return null;
+    }
   };
 
   return (
     <Wrapper>
       <div className='w-full dark:text-white'>
         <div className='p-4'>
-          <CustomSelect options={spaceOptions} onSelect={handleSelect} defaultOption='Personal' />
+          <CustomSelect options={spaceOptions} onSelect={handleSelect} defaultOption={selectedSpace} />
         </div>
-        <div className='max-sm:mx-4 mx-4 h-min flex rounded-xl p-8 max-sm:p-5 bg-neutral-200 dark:bg-neutral-900'>
-          <div className='w-full'>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
-              <div className='flex flex-col justify-center h-28'>
-                <span className='text-3xl font-extrabold text-neutral-900 dark:text-white md:text-5xl lg:text-6xl'>
-                  <span>Welcome, {user?.name}</span>
-                </span>
-                <p className='text-lg font-normal text-neutral-500 lg:text-xl dark:text-neutral-400'>
-                  Unveil Your World, Connect Your Dreams - Where Privacy Meets Possibility.
-                </p>
-              </div>
-              <div className='bg-neutral-800 rounded-lg' />
-            </div>
-            <div className='grid grid-cols-1 md:grid-cols-4 gap-8 md:pt-8'>
-              <TaskWidget />
-              <EventWidget />
-            </div>
-          </div>
-        </div>
+        {renderContent()}
       </div>
       <SpeedDial />
     </Wrapper>
---
+++ components/home/spaces/personal.tsx
@@ -0,0 +1,32 @@
+import AuthContext from '@/contexts/AuthContext';
+import { useContext } from 'react';
+import EventWidget from '../widgets/EventWidget';
+import TaskWidget from '../widgets/TaskWidget';
+
+const Personal = () => {
+  let { user } = useContext(AuthContext);
+
+  return (
+    <div className='max-sm:mx-4 mx-4 h-min flex rounded-xl p-8 max-sm:p-5 bg-neutral-200 dark:bg-neutral-900'>
+      <div className='w-full'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+          <div className='flex flex-col justify-center h-28'>
+            <span className='text-3xl font-extrabold text-neutral-900 dark:text-white md:text-5xl lg:text-6xl'>
+              <span>Welcome, {user?.name}</span>
+            </span>
+            <p className='text-lg font-normal text-neutral-500 lg:text-xl dark:text-neutral-400'>
+              Unveil Your World, Connect Your Dreams - Where Privacy Meets Possibility.
+            </p>
+          </div>
+          <div className='bg-neutral-800 rounded-lg' />
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-8 md:pt-8'>
+          <TaskWidget />
+          <EventWidget />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Personal;
