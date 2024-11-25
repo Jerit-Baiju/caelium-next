6d1f7344562dca7b5859e5788da696d3f35397bd
Jerit Baiju <jeritalumkal@gmail.com>
2024-11-25 23:52:21 +0530

    feat(chats): Adds loading indicator to chats UI
    Introduces `isLoading` state and `Loader` component to display a loading indicator while chats are being fetched.
    
    Adjusts the chat pane height to accommodate the loading indicator.

--- components/chats/ChatsPane.tsx
+++ components/chats/ChatsPane.tsx
@@ -5,12 +5,14 @@ import { getTime, truncate } from '@/helpers/support';
 import useAxios from '@/hooks/useAxios';
 import Link from 'next/link';
 import { useContext, useEffect, useState } from 'react';
+import Loader from '../Loader';
 
 const ChatsPane = () => {
   const { authTokens } = useContext(AuthContext);
   const [chats, setChats] = useState<Chat[]>([]);
   const [search_query, setSearch_query] = useState('');
   const api = useAxios();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const searchChats = async (e: any) => {
     e.preventDefault();
@@ -26,6 +28,9 @@ const ChatsPane = () => {
     try {
       const response = await api.get('/api/chats/');
       setChats(response.data);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -42,7 +47,7 @@ const ChatsPane = () => {
   };
 
   return (
-    <div className='flex flex-col h-[calc(100dvh-5rem)] w-full flex-grow overflow-hidden overflow-x-hidden'>
+    <div className='flex flex-col h-[calc(100dvh-10rem)] w-full flex-grow overflow-hidden overflow-x-hidden'>
       <div className='flex w-full sticky top-0 z-10 flex-col'>
         <form onSubmit={searchChats} className='m-3'>
           <label htmlFor='default-search' className='mb-2 text-sm font-medium text-neutral-900 sr-only dark:text-white'>
@@ -81,34 +86,40 @@ const ChatsPane = () => {
           </div>
         </form>
       </div>
-      <ul role='list' className='flex flex-col overflow-y-auto'>
-        {chats.map((chat: Chat) => (
-          <Link key={chat.id} href={`/chats/${chat.id}`}>
-            <li className='flex items-center justify-between px-3 py-2 m-1 rounded-md hover:bg-neutral-200 dark:hover:bg-neutral-900'>
-              <div className='flex items-center space-x-3 rtl:space-x-reverse'>
-                <div className='flex-shrink-0 dark:bg-white rounded-full'>
-                  <img
-                    className='w-12 h-12 rounded-full border dark:border-neutral-500 border-neutral-200 object-cover'
-                    src={chat.other_participant.avatar}
-                    alt={chat.other_participant.name}
-                  />
+      {isLoading ? (
+        <div className='flex flex-grow items-center justify-center'>
+          <Loader />
+        </div>
+      ) : (
+        <ul role='list' className='flex flex-col overflow-y-auto'>
+          {chats.map((chat: Chat) => (
+            <Link key={chat.id} href={`/chats/${chat.id}`}>
+              <li className='flex items-center justify-between px-3 py-2 m-1 rounded-md hover:bg-neutral-200 dark:hover:bg-neutral-900'>
+                <div className='flex items-center space-x-3 rtl:space-x-reverse'>
+                  <div className='flex-shrink-0 dark:bg-white rounded-full'>
+                    <img
+                      className='w-12 h-12 rounded-full border dark:border-neutral-500 border-neutral-200 object-cover'
+                      src={chat.other_participant.avatar}
+                      alt={chat.other_participant.name}
+                    />
+                  </div>
+                  <div className='flex-1 min-w-0'>
+                    <p className='text-sm font-semibold text-neutral-900 truncate dark:text-white'>{chat.other_participant.name}</p>
+                    <span className='text-sm text-neutral-500 dark:text-neutral-400'>
+                      {truncate({ chars: chat.last_message_content, length: 45 })}
+                    </span>
+                  </div>
                 </div>
-                <div className='flex-1 min-w-0'>
-                  <p className='text-sm font-semibold text-neutral-900 truncate dark:text-white'>{chat.other_participant.name}</p>
-                  <span className='text-sm text-neutral-500 dark:text-neutral-400'>
-                    {truncate({ chars: chat.last_message_content, length: 45 })}
+                {chat.updated_time && (
+                  <span className='inline-flex items-end bg-neutral-300 text-neutral-800 text-sm font-medium px-2.5 py-0.5 rounded-full dark:bg-neutral-900 dark:text-neutral-300'>
+                    {getTime(chat.updated_time)}
                   </span>
-                </div>
-              </div>
-              {chat.updated_time && (
-                <span className='inline-flex items-end bg-neutral-300 text-neutral-800 text-sm font-medium px-2.5 py-0.5 rounded-full dark:bg-neutral-900 dark:text-neutral-300'>
-                  {getTime(chat.updated_time)}
-                </span>
-              )}
-            </li>
-          </Link>
-        ))}
-      </ul>
+                )}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
