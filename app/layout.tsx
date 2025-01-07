7005a5bebdd55e3c85d3b1626a3a1aadf3dfddd1
Jerit Baiju <jeritalumkal@gmail.com>
2025-01-07 16:06:55 +0530

    refactor(layout): simplify HTML structure and improve viewport handling in RootLayout and ChatMain components

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,10 +26,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <head>
         <meta name='mobile-web-app-capable' content='yes' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'></meta>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0 user-scalable=no'/>
         <link
           rel='stylesheet'
           href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0'
@@ -37,12 +37,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'></link>
       </head>
       <body className={`${handleeFont.className} dark:bg-neutral-950 bg-white dark:text-neutral-200 text-neutral-800`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
           <ProvidersWrapper>{children}</ProvidersWrapper>
         </ThemeProvider>
         <Script src='https://kit.fontawesome.com/c75f557ffd.js' crossOrigin='anonymous'></Script>
