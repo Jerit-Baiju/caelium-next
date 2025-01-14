c7b83661db0172fea3019de67ed17ed135f0825b
Jerit Baiju <jeritalumkal@gmail.com>
2025-01-14 09:58:31 +0530

    refactor(chats): improve ChatHeader structure and enhance link accessibility

--- components/chats/elements/ChatHeader.tsx
+++ components/chats/elements/ChatHeader.tsx
@@ -1,4 +1,3 @@
-import { useContext, useEffect, useRef, useState } from 'react';
 import {
   AlertDialog,
   AlertDialogAction,
@@ -14,6 +13,7 @@ import AuthContext from '@/contexts/AuthContext';
 import ChatContext from '@/contexts/ChatContext';
 import { NavLink } from '@/helpers/props';
 import Link from 'next/link';
+import { useContext, useEffect, useRef, useState } from 'react';
 
 const ChatHeader = () => {
   const [isAlertOpen, setIsAlertOpen] = useState(false);
@@ -49,9 +49,9 @@ const ChatHeader = () => {
     <>
       <div ref={headerRef} className='flex sticky top-0 z-10 flex-row h-16 bg-neutral-300 dark:bg-neutral-900 dark:text-white'>
         <Link className='flex flex-col my-auto self-start p-3 h-min justify-center rounded-full' href='/chats'>
-          <i className='fa-solid fa-arrow-left'></i>
+          <i className='fa-solid fa-arrow-left ms-2'></i>
         </Link>
-        <div className='flex items-center'>
+        <Link href={`/chats/${meta?.id}/info`} className='flex flex-grow items-center cursor-default'>
           {!meta?.is_group ? (
             <img
               className='h-12 my-2 w-12 max-sm:h-12 max-sm:w-12 rounded-full dark:bg-white object-cover'
@@ -73,34 +73,32 @@ const ChatHeader = () => {
               <i className='fa-solid fa-people-group text-2xl'></i>
             </div>
           )}
-          <Link href={`/chats/${meta?.id}/info`}>
-            <div className='flex flex-col ps-2'>
-              <div className='flex items-center gap-2'>
-                <p className='text-2xl'>
-                  {meta?.is_group
-                    ? meta.name
-                    : getParticipant(meta?.participants.find((participant) => participant.id !== user.id)?.id ?? 0)?.name}
-                </p>
-              </div>
-              {meta?.is_group ? (
-                <p className='text-sm text-neutral-600 dark:text-neutral-400 truncate max-w-[200px] sm:max-w-[300px] md:max-w-[400px] lg:max-w-[600px] xl:max-w-[800px]'>
-                  {meta.participants
-                    .slice(0, window.innerWidth > 1024 ? 6 : 2)
-                    .map((p) => getParticipant(p.id)?.name)
-                    .filter(Boolean)
-                    .join(', ')}
-                  {meta.participants.length > (window.innerWidth > 1024 ? 6 : 2) &&
-                    ` + ${meta.participants.length - (window.innerWidth > 1024 ? 6 : 2)} others`}
-                </p>
-              ) : (
-                <span className='text-sm'>
-                  {getLastSeen(meta?.participants.find((participant) => participant.id !== user.id)?.id ?? 0)}
-                </span>
-              )}
+          <div className='flex flex-col ps-2'>
+            <div className='flex items-center gap-2'>
+              <p className='text-2xl'>
+                {meta?.is_group
+                  ? meta.name
+                  : getParticipant(meta?.participants.find((participant) => participant.id !== user.id)?.id ?? 0)?.name}
+              </p>
             </div>
-          </Link>
-        </div>
-        <div className='flex items-center flex-grow justify-end'>
+            {meta?.is_group ? (
+              <p className='text-sm text-neutral-600 dark:text-neutral-400 truncate max-w-[200px] sm:max-w-[300px] md:max-w-[400px] lg:max-w-[600px] xl:max-w-[800px]'>
+                {meta.participants
+                  .slice(0, window.innerWidth > 1024 ? 6 : 2)
+                  .map((p) => getParticipant(p.id)?.name)
+                  .filter(Boolean)
+                  .join(', ')}
+                {meta.participants.length > (window.innerWidth > 1024 ? 6 : 2) &&
+                  ` + ${meta.participants.length - (window.innerWidth > 1024 ? 6 : 2)} others`}
+              </p>
+            ) : (
+              <span className='text-sm'>
+                {getLastSeen(meta?.participants.find((participant) => participant.id !== user.id)?.id ?? 0)}
+              </span>
+            )}
+          </div>
+        </Link>
+        <div className='flex items-center justify-end'>
           <DropdownMenu>
             <DropdownMenuTrigger className='outline-none'>
               <i className='fa-solid fa-ellipsis-vertical p-3 me-4'></i>
