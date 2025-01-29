4b783fb41f4de75d9cb73393cbc3744ae49c3e5f
Jerit Baiju <jeritalumkal@gmail.com>
2025-01-29 15:41:57 +0530

    refactor(home): update Home component to manage navbar visibility and clean up imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,21 @@
 'use client';
-import Personal from '@/components/home/spaces/personal';
+// import Personal from '@/components/home/spaces/personal';
 // import SpeedDial from '@/components/home/SpeedDial';
 import Loader from '@/components/Loader';
 import AuthContext from '@/contexts/AuthContext';
-import { useContext } from 'react';
-
-/**
- * Home component temporarily blocks the homepage and redirects to the '/chats' route.
- * It manages the selected space state and persists it in localStorage.
- * Depending on the selected space, it renders different content.
- * If the user is not authenticated, it shows a loader.
- */
+import { useNavbar } from '@/contexts/NavContext';
+import { useContext, useEffect } from 'react';
 
 export default function Home() {
   const { user } = useContext(AuthContext);
+  const { setShowNav } = useNavbar();
+
+  useEffect(() => {
+    setShowNav(false);
+  }, []);
 
   return user ? (
-    <div className='w-full dark:text-white'>
-      {/* <Personal /> */}
-    </div>
+    <div className='w-full dark:text-white'>{/* <Personal /> */}</div>
   ) : (
     <div className='flex items-center justify-center h-screen'>
       <Loader />
