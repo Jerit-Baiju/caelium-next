f362e67329fc1f778fc67583e9d530f0146fcf07
Jerit Baiju <jeritalumkal@gmail.com>
2025-03-23 12:16:24 +0530

    feat(cloud-layout): simplify CloudLayout component structure and improve return JSX

--- app/cloud/layout.tsx
+++ app/cloud/layout.tsx
@@ -3,18 +3,14 @@
 import { useNavbar } from '@/contexts/NavContext';
 import { useEffect } from 'react';
 
-export default function CloudLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function CloudLayout({ children }: { children: React.ReactNode }) {
   const { setCloudNav, resetToDefaultNav, setCloudSidebar, resetToDefaultSidebar } = useNavbar();
 
   useEffect(() => {
     // Set cloud navigation and sidebar when the layout mounts
     setCloudNav();
     setCloudSidebar();
-    
+
     // Reset to default navigation and sidebar when the layout unmounts
     return () => {
       resetToDefaultNav();
@@ -22,11 +18,5 @@ export default function CloudLayout({
     };
   }, [setCloudNav, resetToDefaultNav, setCloudSidebar, resetToDefaultSidebar]);
 
-  return (
-    <section className="cloud-container">
-      <div className="cloud-content">
-        {children}
-      </div>
-    </section>
-  );
+  return <main className='flex grow md:p-6 md:gap-6 md:h-[calc(100vh-5rem)] justify-center overflow-auto'>{children}</main>;
 }
